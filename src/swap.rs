fcbbd6d428012a2cf9706b8b505c81aa9286f195
Jeff DeLonge <jeff@dflow.net>
2025-11-24 18:08:15 -0500

    fix: add default Swap variant

--- src/swap.rs
+++ src/swap.rs
@@ -1,3 +1,6 @@
 /// This enum is a dflow backend enum and does not map 1:1 to the onchain aggregator Swap enum
-#[derive(Clone, PartialEq, Debug)]
-pub enum Swap {}
+#[derive(Debug, Clone, Default, PartialEq)]
+pub enum Swap {
+    #[default]
+    Placeholder,
+}
